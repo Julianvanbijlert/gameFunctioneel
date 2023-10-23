9ed37b24b723f393c4ff4804eb88a6fc9aa13e50
Julianvanbijlert <116498773+Julianvanbijlert@users.noreply.github.com>
2023-10-23 18:21:32 +0200

    ff up down en spacebar toegevoegd aan keys, ook probleem gefixt met dubbel klikken

--- app/Controller.hs
+++ app/Controller.hs
@@ -8,10 +8,14 @@ import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
 import System.Random
 
+import Graphics.Gloss.Data.Picture
+
+import Graphics.Gloss.Data.Color
+
 -- | Handle one iteration of the game
 step :: Float -> GameState -> IO GameState
 step secs gstate@(GameState(InfoToShow b p xs bs) _)
-  | elapsedTime gstate + secs > nO_SECS_BETWEEN_CYCLES
+  | elapsedTime gstate + secs > numberOfSecsBetweenActions
   = 
     return $ moveEverything gstate { elapsedTime = 0 }
     
@@ -33,8 +37,8 @@ moveAllEnemies (e@(Rock (Point(x, y)) (Vector(dx, dy))) : xs) =  Rock (Point(x +
 
 moveAllBullets :: [Bullet] -> [Bullet]
 moveAllBullets [] = []
-moveAllBullets ((EnemyBullet  (Point(x,y)) (Vector(dx, dy))) : xs) = (EnemyBullet  (Point(x + dx, y + dy)) (Vector(dx, dy))) : moveAllBullets xs
-moveAllBullets ((PlayerBullet (Point(x,y)) (Vector(dx, dy))) : xs)   = (PlayerBullet (Point(x + dx, y + dy)) (Vector(dx, dy))) : moveAllBullets xs  --moet nog vervangen worden maar voor nu ff
+moveAllBullets ((EnemyBullet  (Point(x,y)) (Vector(dx, dy))) : xs) = EnemyBullet  (Point(x + dx, y + dy)) (Vector(dx, dy)) : moveAllBullets xs
+moveAllBullets ((PlayerBullet (Point(x,y)) (Vector(dx, dy))) : xs)   = PlayerBullet (Point(x + dx, y + dy)) (Vector(dx, dy)) : moveAllBullets xs  --moet nog vervangen worden maar voor nu ff
 
 -- | Handle user input
 input :: Event -> GameState -> IO GameState
@@ -66,13 +70,17 @@ shootBulletE = undefined
 
 --if spaceship hits a wall it will go back in the screen, if rock does this it breaks
 hittWall :: Enemy -> Enemy
-hittWall (SpaceShip p (Vector(dx, dy))) = (SpaceShip p (Vector(dx, -dy)))
+hittWall (SpaceShip p (Vector(dx, dy))) = SpaceShip p (Vector(dx, -dy))
 hittWall rock = destroyEnemy rock
 
 inputKey :: Event -> GameState -> GameState 
-inputKey (EventKey (Char c) _ _ _) gstate@(GameState i e) 
+inputKey (EventKey (Char c) Down _  _) gstate@(GameState i e) 
   = -- If the user presses a character key, show that one
     gstate { infoToShow = handleInput c i }
+       
+inputKey (EventKey (SpecialKey k) Down _ _) gstate@(GameState i e) 
+  = 
+    gstate {infoToShow = handleInputSpecial k i}
 inputKey _ gstate = gstate -- Otherwise keep the same
 
 {-Dit is een functie die inputs handled, alleen moet er nog gefixt worden dat ingedrukt houden 
@@ -80,5 +88,11 @@ meerdere interacties doet en dat het niet dubbel aangeroepen is als het omhoog g
 handleInput :: Char -> InfoToShow -> InfoToShow
 handleInput 'w' (InfoToShow b (Player (Point(x, y)) (Vector(dx, dy))) e bul) = InfoToShow b (Player (Point(x, y + dy)) (Vector(dx, dy))) e bul
 handleInput 's' (InfoToShow b (Player (Point(x, y)) (Vector(dx, dy))) e bul) = InfoToShow b (Player (Point(x, y - dy)) (Vector(dx, dy))) e bul
-handleInput 'f' (InfoToShow b p@(Player (Point(x, y)) _ ) e bul) = InfoToShow b p e ((PlayerBullet (Point(x + 40, y)) (Vector(5, 0))) : bul)
+handleInput 'f' (InfoToShow b p@(Player (Point(x, y)) _ ) e bul) = InfoToShow b p e (PlayerBullet (Point(x + 40, y)) (Vector(5, 0)) : bul)
 handleInput _ i = i
+
+handleInputSpecial :: SpecialKey -> InfoToShow -> InfoToShow
+handleInputSpecial KeyUp    (InfoToShow b (Player (Point(x, y)) (Vector(dx, dy))) e bul) = InfoToShow b (Player (Point(x, y + dy)) (Vector(dx, dy))) e bul
+handleInputSpecial KeyDown  (InfoToShow b (Player (Point(x, y)) (Vector(dx, dy))) e bul) = InfoToShow b (Player (Point(x, y - dy)) (Vector(dx, dy))) e bul
+handleInputSpecial KeySpace (InfoToShow b p@(Player (Point(x, y)) _ ) e bul) = InfoToShow b p e (PlayerBullet (Point(x + 40, y)) (Vector(5, 0)) : bul)
+handleInputSpecial _ i = i
